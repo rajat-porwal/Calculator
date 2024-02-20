289e74206d2c36e200fb4c60585881132c95fc7c
Rajat Porwal <rajatporwal@protonmail.com>
2024-02-20 18:01:48 +0530

    feat: create a function operate which calls other airthmatic operation function as per operator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let operand1, operand2, operator = null;
+
+
+
 function sum(Num1, Num2){                       
     return Num1 + Num2; 
 }
@@ -11,11 +15,35 @@ function multiplication(Num1, Num2){
 }
 
 function division(numerator, denominator){
-    if( ( !denominator == 0 || isNaN(denominator) ) ){             // condition for denomentor to be not Zero or NaN
+    if( !( denominator == 0 || isNaN(denominator) ) ){             // condition for denominator to be not Zero or NaN
         return numerator / denominator;
     }
-
     else return null;
 }
 
+function operate(operand1, operator, operand2){
+    switch(operator){
+        case '+':
+           sum(operand1, operand2);
+           break;
 
+
+        case '-':
+            subtract(operand1, operand2);
+            break;
+            
+        case '*':
+            multiplication(operand1, operand2);
+            break;
+
+        case '/':
+            division(operand1, operand2);
+            break;
+            
+        default:
+            alert('Invalid Selection');
+    }
+
+}
+
+operate(5, '/', 61);
